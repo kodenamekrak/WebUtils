9cf6fb14e8d6c254e712a34a33df75bf11b7252e
RedBrumbler <redbrumbler.official+github@gmail.com>
2024-04-23 20:02:31 +0200

    Setup SSL

--- src/DownloaderUtility.cpp
+++ src/DownloaderUtility.cpp
@@ -71,7 +71,8 @@ namespace WebUtils {
 
         std::string userAgent = urlOptions.userAgent.value_or(userAgent);
         curl_easy_setopt(curl, CURLOPT_USERAGENT, userAgent.c_str());
-        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, urlOptions.useSSL);
+        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, urlOptions.useSSL);
 
         response->CurlStatus = curl_easy_perform(curl);
         if (response->CurlStatus == CURLE_OK) response->AcceptData(recvData);
@@ -142,7 +143,8 @@ namespace WebUtils {
 
         std::string userAgent = urlOptions.userAgent.value_or(userAgent);
         curl_easy_setopt(curl, CURLOPT_USERAGENT, userAgent.c_str());
-        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, urlOptions.useSSL);
+        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, urlOptions.useSSL);
 
         auto curlStatus = curl_easy_perform(curl);
         int httpCode = 0;
